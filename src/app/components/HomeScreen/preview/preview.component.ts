ca724a3be5781ce3330c7df4566e6d48176b486a
angie932 <tapiieroangii@gmail.com>
2022-06-02 17:42:53 -0500

    se termina el mensaje de alerta

--- src/app/components/HomeScreen/preview/preview.component.ts
+++ src/app/components/HomeScreen/preview/preview.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ProfileService } from 'src/app/services/HttpServices/profile.service';
 import { LoadscriptsService } from 'src/app/services/InterfaceServices/loadscripts.service';
 import { AdsService } from '../../../services/HttpServices/ads.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-preview',
@@ -11,7 +12,7 @@ import { AdsService } from '../../../services/HttpServices/ads.service';
 export class PreviewComponent implements OnInit {
   public ad: Array<any> = [0]
 
-  constructor(private _LoadScripts:LoadscriptsService,private ads:AdsService) { 
+  constructor(private _LoadScripts:LoadscriptsService,private ads:AdsService) {
     _LoadScripts.Charge(["HomeScreen/preview/functions"])
 
     this.ads.getAds().subscribe((response: any)=>{
@@ -22,4 +23,21 @@ export class PreviewComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  showModal(){
+    Swal.fire({
+      icon: 'error',
+      title: 'Oops...',
+      text: '¡No has iniciado sesion, ingresa para acceder a esta funcion!',
+      footer: '<a href=""></a><a href="/SignUp" style="color: #fff; text-decoration: none; background-color: #098ab5; padding: 9px;border-radius:5px;border:4px solid #03d3fa;">Registrarse</a><div style="width:10%;"></div><a href="/SignIn" style="color: #fff; text-decoration: none; background-color: #7066e0; padding: 9px; border-radius:5px;border:4px solid #a49df3;">Iniciar sesion</a>',
+      showCancelButton: false,
+      showConfirmButton: false,
+      showClass: {
+        popup: 'animate__animated animate__fadeInDown'
+      },
+      hideClass: {
+        popup: 'animate__animated animate__fadeOutUp'
+      }
+    })
+  }
 }
+
